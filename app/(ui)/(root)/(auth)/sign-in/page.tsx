608141b8fd90bd9c5cf402b22ff486b314442683
Fredo-Ronan <fredocode06@gmail.com>
2024-04-17 20:30:28 +0700

    clean warning codes

--- app/(ui)/(root)/(auth)/sign-in/page.tsx
+++ app/(ui)/(root)/(auth)/sign-in/page.tsx
@@ -122,7 +122,7 @@ const CustomerLoginPage = async () => {
             <div className="mt-6">
               <button
                 type="submit"
-                className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
+                className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
               >
                 Login
               </button>
