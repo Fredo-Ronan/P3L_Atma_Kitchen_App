544b8654c715f33c59e9c6f87751b093d6423913
Fredo-Ronan <fredocode06@gmail.com>
2024-05-07 12:15:03 +0700

    fix error production 3

--- app/adminView/hampers/[...detail_hampers]/page.tsx
+++ app/adminView/hampers/[...detail_hampers]/page.tsx
@@ -45,7 +45,7 @@ const DetailHampersAdminPage = ({ params }: { params: { detail_hampers: string[]
 
   return (
     <div>
-        <h1 className="text-2xl font-bold">Detail Hampers "{nama_hampers}"</h1>
+        <h1 className='text-2xl font-bold'>Detail Hampers "{nama_hampers}"</h1>
         <div className='mt-2 mb-4'>
           <p className='text-lg opacity-70 italic'>Keterangan : </p>
           <p className='text-lg'>Exclusive box and Card : {dataHampers?.DESKRIPSI_HAMPERS.includes('+ Exclusive box and Card') ? "Ya (+ Rp. 50.000)" : "Tidak"}</p>
