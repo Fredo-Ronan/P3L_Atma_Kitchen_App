4b88f72d31b86c41dc022e936d6b734c1ef807c8
Aditya-PutraAP <adityaputra.ap543@gmail.com>
2024-05-10 19:40:13 +0700

    test

--- app/api/presensiKaryawan/[id]/route.ts
+++ app/api/presensiKaryawan/[id]/route.ts
@@ -7,7 +7,8 @@ export async function PUT(
 ) {
   try {
     const connection = await connect();
-    const { status_presensi } = await req.json();
+    const res = await req.json();
+    const { status_presensi } = JSON.parse(res.body);
 
     const [rows, fields] = await connection.execute(
       `UPDATE PRESENSI_KARYAWAN SET STATUS_PRESENSI = '${status_presensi}' WHERE ID_PRESENSI_KARYAWAN = ${params.id}`
