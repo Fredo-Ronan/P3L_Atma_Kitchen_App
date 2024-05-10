7bb89400bb7a535467dd9aac2a7ffc187d43fc02
Aditya-PutraAP <adityaputra.ap543@gmail.com>
2024-05-10 13:43:23 +0700

    fixing bug

--- app/api/presensiKaryawan/route.ts
+++ app/api/presensiKaryawan/route.ts
@@ -1,19 +1,21 @@
 import { connect } from "@/db";
 import { NextRequest, NextResponse } from "next/server";
+import { parseResultQuery } from "@/utilities/resultQueryParser";
+import { StatusCodesP3L } from "@/constants/statusCodesP3L";
 
 export async function POST(req: NextRequest) {
   try {
     const connection = await connect();
-    const [karyawanRows] = await connection.query('SELECT * FROM KARYAWAN');
-    
-    const karyawanData = karyawanRows as any[];
-    const values = karyawanData.map((row: any) => [row.ID_KARYAWAN, 'Hadir']);
+    const [karyawanRows] = await connection.query("SELECT * FROM KARYAWAN");
 
-    const placeholders = values.map(() => '(?, CURDATE(), ?)');
+    const karyawanData = karyawanRows as any[];
+    const values = karyawanData.map((row: any) => [row.ID_KARYAWAN, "Hadir"]);
+
+    const placeholders = values.map(() => "(?, CURDATE(), ?)");
     const flattenedValues = values.flat();
 
     const query = `INSERT INTO PRESENSI_KARYAWAN (ID_KARYAWAN, TANGGAL_PRESENSI, STATUS_PRESENSI) VALUES ${placeholders}`;
-    
+
     const [rows] = await connection.query(query, flattenedValues);
 
     await connection.end();
@@ -27,47 +29,36 @@ export async function POST(req: NextRequest) {
   }
 }
 
-
-
-export async function GET(request: NextRequest) {
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   try {
     const connection = await connect();
 
-    const page = request.nextUrl.searchParams.get("page") || 1;
-    const q = request.nextUrl.searchParams.get("q") || "";
+    const queryGetPresensi = `SELECT * FROM PRESENSI_KARYAWAN AS PK JOIN KARYAWAN AS K ON PK.ID_KARYAWAN = K.ID_KARYAWAN WHERE PK.TANGGAL_PRESENSI = CURRENT_DATE()`;
 
-    if (Number(page) <= 0 || isNaN(Number(page))) {
-      connection.end();
-      return NextResponse.json({ data: [], totalData: 0 });
-    }
-
-    let query = "SELECT * FROM PRESENSI_KARYAWAN AS PK JOIN KARYAWAN AS K ON PK.ID_KARYAWAN = K.ID_KARYAWAN WHERE PK.TANGGAL_PRESENSI = CURRENT_DATE()";
-
-    if (q) {
-      query += ` AND K.NAMA_KARYAWAN LIKE '%${q}%'`;
-    }
-
-    let totalData = 0;
-
-    const offset = (Number(page) - 1) * 10;
-
-    const [rows] = await connection.execute(query);
-
-    if (Array.isArray(rows)) {
-      if (rows.length === 0) {
-        connection.end();
-        return NextResponse.json({ data: [], totalData: 0 });
-      }
-      totalData = rows.length;
-    }
-
-    query += ` LIMIT 10 OFFSET ${offset}`;
-
-    const [data] = await connection.execute(query);
-
+    const [resultGetPresensi, fields] = await connection.execute(
+      queryGetPresensi
+    );
     connection.end();
 
-    return NextResponse.json({ data, totalData }, { status: 200 });
+    const final_result_presensi = parseResultQuery(resultGetPresensi);
+
+    const data_presensi = {
+      id_presensi_karyawan: JSON.parse(final_result_presensi)
+        .ID_PRESENSI_KARYAWAN,
+      id_karyawan: JSON.parse(final_result_presensi).ID_KARYAWAN,
+      nama_karyawan: JSON.parse(final_result_presensi).NAMA_KARYAWAN,
+      tanggal_presensi: JSON.parse(
+        final_result_presensi
+      ).TANGGAL_PRESENSI.split("T")[0],
+      status_presensi: JSON.parse(final_result_presensi).STATUS_PRESENSI,
+    };
+
+    return new Response(
+      JSON.stringify({ status: StatusCodesP3L.OK, data: data_presensi })
+    );
   } catch (error) {
     console.log(error);
     throw error;
