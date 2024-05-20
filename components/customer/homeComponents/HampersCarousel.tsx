411a21ace10cdc96fc92e5c45010fc42a4a56db1
Fredo-Ronan <fredocode06@gmail.com>
2024-05-20 20:33:06 +0700

    try fix UI bug again

--- components/customer/homeComponents/HampersCarousel.tsx
+++ components/customer/homeComponents/HampersCarousel.tsx
@@ -14,8 +14,8 @@ import { HAMPERS, PRODUK } from "@/types";
 import { formatNumber } from "@/utilities/formatAngkaHarga";
 
 interface DETIL_GAMBAR {
-    nama_hampers: string;
-    gambar_produk: PRODUK[];
+  nama_hampers: string;
+  gambar_produk: PRODUK[];
 }
 
 const HampersCarousel = ({ dataHampers }: { dataHampers: HAMPERS[] }) => {
@@ -26,7 +26,6 @@ const HampersCarousel = ({ dataHampers }: { dataHampers: HAMPERS[] }) => {
   const [gambar, setGambar] = useState<DETIL_GAMBAR[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  // untuk ambil gambar produk aka. link gambar produk yang ada di dalam hampers tertentu (ID_HAMPERS) dari tabel produk
   const getGambar = async () => {
     setIsLoading(true);
     try {
@@ -35,7 +34,7 @@ const HampersCarousel = ({ dataHampers }: { dataHampers: HAMPERS[] }) => {
         const resFinal = await resGambar.json();
         return { nama_hampers: data.NAMA_HAMPERS, gambar_produk: resFinal.detilHampers };
       });
-  
+
       const allGambar = await Promise.all(gambarPromises);
       setGambar(allGambar);
     } catch (error) {
@@ -47,7 +46,7 @@ const HampersCarousel = ({ dataHampers }: { dataHampers: HAMPERS[] }) => {
 
   useEffect(() => {
     getGambar();
-  }, [])
+  }, []);
 
   return (
     <div>
@@ -58,49 +57,71 @@ const HampersCarousel = ({ dataHampers }: { dataHampers: HAMPERS[] }) => {
         onMouseLeave={plugin.current.reset}
       >
         <CarouselContent>
-          {dataHampers?.map((data, index) => (
+          {dataHampers.map((data, index) => (
             <CarouselItem key={index}>
-                {
-                  isLoading ?
-                  <Skeleton className="w-full h-96"/>
-                  :
-                  <div className="bg-slate-400 relative" key={index}>
-                    <img src="/assets/images/banner hampers.png" alt="" />
-                    <h1 className="absolute top-36 bottom-0 left-16 right-0 text-5xl font-poetsen font-bold">{data.NAMA_HAMPERS}</h1>
-                    <h1 className="absolute top-52 bottom-0 left-16 right-0 text-2xl font-poetsen">{data.DESKRIPSI_HAMPERS.split("+")[0]}</h1>
+              {isLoading ? (
+                <Skeleton className="w-full h-96" />
+              ) : (
+                <div className="bg-slate-400 relative">
+                  <img src="/assets/images/banner hampers.png" alt="" />
+                  <h1 className="absolute top-36 bottom-0 left-16 right-0 text-5xl font-poetsen font-bold">
+                    {data.NAMA_HAMPERS}
+                  </h1>
+                  <h1 className="absolute top-52 bottom-0 left-16 right-0 text-2xl font-poetsen">
+                    {data.DESKRIPSI_HAMPERS.split("+")[0]}
+                  </h1>
 
-                    {data.DESKRIPSI_HAMPERS.includes("+ Exclusive") ? 
-                        <p className="before:block before:absolute before:-inset-2 before:-skew-y-2 before:w-72 before:h-12 before:-z-40 before:bg-blue-500 before:shadow-lg absolute top-60 bottom-0 left-20 right-0 transform -rotate-6 text-2xl font-poetsen text-white">+ {data.DESKRIPSI_HAMPERS.split("+")[1]}</p>
-                        : <></>
-                    }
-                    
-                    <div className="before:block before:absolute before:w-44 before:h-20 before:rounded-full before:-z-40 before:bg-yellow-500 before:shadow-xl absolute -top-40 bottom-0 right-[720px] z-50 flex justify-center items-center transform rotate-6">
-                        <p className="text-white text-2xl font-bold font-poetsen">Rp. {formatNumber(data.HARGA_HAMPERS)}</p>
-                    </div>
+                  {data.DESKRIPSI_HAMPERS.includes("+ Exclusive") ? (
+                    <p className="before:block before:absolute before:-inset-2 before:-skew-y-2 before:w-72 before:h-12 before:-z-40 before:bg-blue-500 before:shadow-lg absolute top-60 bottom-0 left-20 right-0 transform -rotate-6 text-2xl font-poetsen text-white">
+                      + {data.DESKRIPSI_HAMPERS.split("+")[1]}
+                    </p>
+                  ) : null}
 
-                    <div className="absolute top-0 bottom-0 right-52" key={data.ID_HAMPERS}>
-                        {gambar.map((dataGambar, index) => (
-                            dataGambar.nama_hampers === data.NAMA_HAMPERS ? 
-                            dataGambar.gambar_produk.map((gambar, indexGambar) => (
-                                indexGambar % 2 === 0 ?
-                                <div className="relative transform rotate-6 top-16 shadow-xl group w-[250px]" key={gambar.GAMBAR_PRODUK}>
-                                    <div className="absolute text-black text-center p-2 text-2xl w-full font-bold font-poetsen bg-white transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-                                        {gambar.NAMA_PRODUK}
-                                    </div>
-                                    <img src={gambar.GAMBAR_PRODUK} className="border-8 border-white" alt=""/>
-                                </div>
-                                :
-                                <div className="relative transform -rotate-6 -top-20 right-48 shadow-xl group w-[210px]" key={gambar.GAMBAR_PRODUK}>
-                                    <div className="absolute text-black text-center p-2 text-2xl w-full font-bold font-poetsen bg-white transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-                                        {gambar.NAMA_PRODUK}
-                                    </div>
-                                    <img src={gambar.GAMBAR_PRODUK} className="border-8 border-white" alt=""/>
-                                </div>
-                            )) : <></>
-                        ))}
-                    </div>
+                  <div className="before:block before:absolute before:w-44 before:h-20 before:rounded-full before:-z-40 before:bg-yellow-500 before:shadow-xl absolute -top-40 bottom-0 right-[720px] z-50 flex justify-center items-center transform rotate-6">
+                    <p className="text-white text-2xl font-bold font-poetsen">
+                      Rp. {formatNumber(data.HARGA_HAMPERS)}
+                    </p>
+                  </div>
+
+                  <div className="absolute top-0 bottom-0 right-52">
+                    {gambar.map((dataGambar, dataGambarIndex) =>
+                      dataGambar.nama_hampers === data.NAMA_HAMPERS ? (
+                        dataGambar.gambar_produk.map((gambarItem, gambarIndex) =>
+                          gambarIndex % 2 === 0 ? (
+                            <div
+                              className="relative transform rotate-6 top-16 shadow-xl group w-[250px]"
+                              key={`${dataGambarIndex}-${gambarItem.ID_PRODUK}`}
+                            >
+                              <div className="absolute text-black text-center p-2 text-2xl w-full font-bold font-poetsen bg-white transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+                                {gambarItem.NAMA_PRODUK}
+                              </div>
+                              <img
+                                src={gambarItem.GAMBAR_PRODUK}
+                                className="border-8 border-white"
+                                alt=""
+                              />
+                            </div>
+                          ) : (
+                            <div
+                              className="relative transform -rotate-6 -top-20 right-48 shadow-xl group w-[210px]"
+                              key={`${dataGambarIndex}-${gambarItem.ID_PRODUK}`}
+                            >
+                              <div className="absolute text-black text-center p-2 text-2xl w-full font-bold font-poetsen bg-white transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+                                {gambarItem.NAMA_PRODUK}
+                              </div>
+                              <img
+                                src={gambarItem.GAMBAR_PRODUK}
+                                className="border-8 border-white"
+                                alt=""
+                              />
+                            </div>
+                          )
+                        )
+                      ) : null
+                    )}
+                  </div>
                 </div>
-                }
+              )}
             </CarouselItem>
           ))}
         </CarouselContent>
